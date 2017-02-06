ec87aebb69419782fd5f6879e2466c9ca073cce8
peguin40 <ang.sweechow@dhs.sg>
2017-02-06 09:07:15 +0800

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,7 +1,9 @@
 def quadratic(arr):
     for i in range(len(arr)):
         curr=arr[i]
-        for j in range(1,len(arr)-i):
-            if curr+arr[i+j]==8:
-                return "Y",str(i),str(i+j)
+        for j in range(len(arr)):
+            if i==j:
+                pass
+            elif curr+arr[j]==8:
+                return "Y",str(i),str(j)
     return "N"
