67e96035903e140562e29de61e91bea6911152c0
peguin40 <ang.sweechow@dhs.sg>
2017-02-11 16:32:31 +0800

    Update linearithmic.py

--- linearithmic.py
+++ linearithmic.py
@@ -1,7 +1,7 @@
-def linearithmic(arr):
+def linearithmic(arr,sum_n):
     for i in range(len(arr)):
         curr=arr[i]
         for j in range(1,len(arr)-i):
-            if curr+arr[i+j]==8:
+            if curr+arr[i+j]==sum_n:
                 return "Y",str(i),str(i+j)
     return "N"
